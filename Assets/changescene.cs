f112bc89ea3f15210b045e0e19590223d5acd2b3
Kevin Lim <kevinlim@dhcp-wifi-8021x-155-41-75-94.bu.edu>
2019-11-17 11:44:50 -0500

    Bose AR project

---
+++ Assets/changescene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour {
+
+	
+
+	void OnTriggerEnter(Collider coll){
+		//if (coll.CompareTag("P1")){
+			SceneManager.LoadScene ("SampleScene");
+		//}
+	}
+
+}
